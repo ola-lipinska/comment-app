fc354d14fba172eefd5895d9200ab99a7cc63a50
ola-lipinska <98603658+ola-lipinska@users.noreply.github.com>
2022-01-31 17:29:32 +0100

    Dodanie pierwszej strony

--- resources/views/content/cities_in_europe.blade.php
+++ resources/views/content/cities_in_europe.blade.php
@@ -3,5 +3,45 @@
 @section('title', 'Comments2- Homepage')
 
 @section('content')
-<p>Miasta Europy</p>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Polska</title>
+  </head>
+  <body>
+<h1>3 miasta w Polsce, które warto odwiedzić</h1>
+<body style="background-color: beige;"></body>
+<p><b>1. Gdańsk</b></p>
+
+<p>Gdańsk jest miastem położonym nad Morzem Bałtyckim, dokładniej nad Zatoką Gdańską, przy ujściu Motławy do Wisły. Urzeka przepiękną, zachwycającą architekturą i niecodzienną atmosferą. Najcenniejsze zabytki położone są wzdłuż Drogi Królewskiej prowadzącej od Bramy Wyżniej do Bramy Zielonej znajdującej się praktycznie przy Motławie. Zwiedzając to miasto na pewno warto zobaczyć: Fontannę Neptuna, Ratusz Głównego Miasta, Bramę Żuraw, Długie Pobrzeże oraz Bazylikę Mariacką. 
+</p>
+<div style="text-align: center"><img src="gdansk.jpg" alt=Tekst"></div>
+
+<p><b>2. Kraków</b></p>
+<p>Kraków jest jednym z najstarszych miast Polski. Posiada drugą w Europie Środkowej, po uniwersytecie w Pradze, najstarszą uczelnię wyższą – Uniwersytet Jagielloński. Do najważniejszych zabytków z całą pewnością należą: Zamek Królewski na Wawelu, mury obronne, Sukiennice, kościół Mariacki, Barbakan, wieża ratuszowa. Kraków urzeka niepowtarzalną atmosferą − chwilami leniwą, chwilami pełną wydarzeń, ale zawsze wyjątkową i inspirującą.</p>
+<div style="text-align: center"><img src="krakow.jpg" alt=Tekst" height="280px" width="482px"></div> 
+
+<p><b>3. Wrocław</b></p>
+<p>Wrocław, stolica Dolnego Śląska, jest jednym z najstarszych i najpiękniejszych miast w Polsce. Położony u podnóża Sudetów, nad rzeką Odrą. Będąc we Wrocławiu nie można ominąć takich miejsc jak: Rynek Główny, Hala Stulecia, Panorama Racławicka, Ogród Japoński. Jest to niezwykle urokliwe miasto, pełne zabytkowych kamienic, o specyficznym klimacie.</p>
+<div style="text-align: center"><img src="wroclaw.jpg" alt=Tekst" height="280px" width="482px"></div
+
+ <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
 @endsection
