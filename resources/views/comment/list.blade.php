4df9e7437248bc860aeb17ea564481074d01bbd5
ola-lipinska <98603658+ola-lipinska@users.noreply.github.com>
2022-01-31 16:39:22 +0100

    fix list

--- resources/views/comment/list.blade.php
+++ resources/views/comment/list.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Comments list')
 
 @section('content')
+    @if($currentComment)
     <div class="row">
         <div class="col col-2">
             <aside>
@@ -47,4 +48,9 @@
             </main>
         </div>
     </div>
+    @else
+    <div class="row">
+        <h1>No comments to display</h1>
+    </div>
+    @endif
 @endsection
