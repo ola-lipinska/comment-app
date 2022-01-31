3e828563e892d5a2808bf94193910f7a19a080bd
ola-lipinska <98603658+ola-lipinska@users.noreply.github.com>
2022-01-31 21:48:40 +0100

    Dodanie

--- resources/views/content/cities_in_europe.blade.php
+++ resources/views/content/cities_in_europe.blade.php
@@ -3,5 +3,45 @@
 @section('title', 'Comments2- Homepage')
 
 @section('content')
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Świat</title>
+  </head>
+  <body>
+<center><h1>3 miasta w Europie, które warto odwiedzić</h1></center>
+<body style="background-color: #CCCCFF;"></body>
+<p><b>1. Ateny</b></p>
+
+<p>Ateny to stolica i jednocześnie największe miasto Grecji. Miasto stanowi jeden z najważniejszych ośrodków turystycznych w Europie i może poszczycić się wieloma zabytkami z czasów starożytnych. Najważniejszym z nich jest oczywiście Akropol.
+</p>
+<div style="text-align: center"><iframe width="560" height="315" src="https://www.youtube.com/embed/sHYrpNEQTOI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+
+<p><b>2. Barcelona</b></p>
+<p>Barcelona to duże miasto, drugie największe po Madrycie w Hiszpanii. Jest stolicą wspólnoty autonomicznej Katalonii. Położona jest nad Morzem Śródziemnym, mniej więcej w środku linii brzegowej Katalonii, wciśnięta pomiędzy morze i góry Serra de Collserola. To bardzo interesujące miasto, tętniące życiem, pełne ciekawych obiektów, szczególnie tych specyficznych dla Barcelony z okresu katalońskiego modernizmu.</p>
+<div style="text-align: center"><iframe width="560" height="315" src="https://www.youtube.com/embed/uOzwwZxjPRs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+
+<p><b>3. Paryż</b></p>
+<p>Miasto zakochanych, stolica światowej mody, jeden z najważniejszych ośrodków turystycznych i kulturalnych. Przybywający tu filozofowie i artyści od wieków kształtowali niepowtarzalną atmosferę tego miasta. Paryż to również miejsce pełne zabytków-symboli, które znają niemal wszyscy. Katedra Notre-Dame de Paris, Luwr, wieża Eiffla, wzgórze Montmartre z bazyliką Sacre Coeur i światowej sławy muzea. A dla najmłodszych parki rozrywki z Disneylandem na czele.</p>
+<div style="text-align: center"><iframe width="560" height="315" src="https://www.youtube.com/embed/1SYziI1df1g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+
+ <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
 @endsection
